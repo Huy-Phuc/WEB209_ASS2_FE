7f3d6d64b36919fb5f2ac2d85d31e810f1b12082
Huy-Phuc <dohuyphuc1443@gmail.com>
2024-07-29 16:51:31 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+// import Footer from "./components/layouts/Footer";
+// import Login from './pages/Login'
+// import Register from './pages/Register'
+import NotFound from "./pages/NotFound";
+// import Header from './components/layouts/Header'
+import ProductComponent from "./pages/Product";
+import FormProduct from "./pages/FormProduct";
+import AuthForm from "./pages/AuthForm";
+import CategoryComponent from "./pages/Category";
+import CategoryForm from "./pages/FormCategory";
+// import CategoryForm from "./pages/FormCategoty";
+// import axios from 'axios'
+// import {User } from './interfaces/User'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Header /> */}
+
+      <main id="main">
+        <Routes>
+          <Route index element={<ProductComponent />} />
+          <Route path="/category" element={<CategoryComponent />} />
+          <Route path="/login" element={<AuthForm />} />
+          <Route path="/register" element={<AuthForm />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/add" element={<FormProduct />} />
+          <Route path="/edit/:id" element={<FormProduct />} />
+          <Route path="/add-cate" element={<CategoryForm />} />
+          <Route path="/edit-cate/:id" element={<CategoryForm />} />
+        </Routes>
+      </main>
+
+      {/* <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ProductProvider } from "./contexts/ProductContext.tsx";
+import { CategoryProvider } from "./contexts/CategoryContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ProductProvider>
+        <CategoryProvider>
+          <App />
+        </CategoryProvider>
+      </ProductProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
